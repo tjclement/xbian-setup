7e19ede5b666250743450892c29e25d5c6cc96e0
Tom Clement <mr@tomclement.nl>
2014-01-05 22:38:06 +0100

    Testing SAMBA and AirPlay fix

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+source settings.sh
+
+PWD=`pwd`
+
+#Check if we're running as root
+if [ "$(id -u)" != "0" ]; then
+  echo "\e[1;31mYou need to run this script as root!\e[0m"
+  exit;
+fi
+
+###FUNCTIONS###
+function message()
+{
+  echo -e "\e[1;32m"
+  echo $1
+  echo -e "\e[0m"
+}
+
+
+##SYSTEM##
+
+message "Changing login credentials for xbian and root"
+echo -e "$XBIAN_PASS\n$XBIAN_PASS" > passwd xbian
+echo -e "$ROOT_PASS\n$ROOT_PASS" > passwd root
+
+message "Changing hostname to '$HOSTNAME'"
+xbian-config hostname update $HOSTNAME
+
+message "Setting timezone to EU/Amsterdam"
+xbian-config timezone update europe amsterdam
+
+message "Configuring video settings"
+xbian-config videoflags update hdmi_force_hotplug disable_overscan disable_splash
+
+message "Creating external HDD symlink folder"
+if [ -n "$EXTERNAL_HDD_SYM_NAME" ]
+then
+	ln -s /media/usb0/ /$EXTERNAL_HDD_SYM_NAME
+fi
+
+message "Copying over usbmount configuration file"
+cp $PWD/etc/usbmount/usbmount.conf /etc/usbmount/usbmount.conf
+
+##SAMBA##
+
+message "Installing ntfs-3g and samba"
+apt-get -qq install ntfs-3g samba samba-common-bin
+
+message "Creating SMB usergroup samba"
+groupadd samba
+
+message "Creating SMB user $SAMBA_USER"
+useradd -M -G samba $SAMBA_USER
+echo -e "$SAMBA_PASS\n$SAMBA_PASS" | (passwd $SAMBA_USER)
+echo -e "$SAMBA_PASS\n$SAMBA_PASS" | (smbpasswd -a -s $SAMBA_USER)
+
+message "Copying over samba configuration file"
+cp $PWD/etc/samba/smb.conf /etc/samba/smb.conf
+
+message "Restarting samba"
+service samba restart
+
+##XBMC##
+message "Copying over XBMC configuration file"
+cp $PWD/home/xbian/.xbmc/userdata/advancedsettings.xml /home/xbian/.xbmc/userdata/advancedsettings.xml
+
+message "Copying over XBMC sources file"
+cp $PWD/home/xbian/.xbmc/userdata/sources.xml /home/xbian/.xbmc/userdata/sources.xml
+
+##AirPlay##
+message "Hotfixing AirPlay functionality"
+echo "frandom" >> /etc/modules
+
+##CLEANUP##
+message "[You're probably going to want to reboot right around now.]"
